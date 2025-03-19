bbd17b2603df7f895710fe03faf1e24d17f476eb
sofyaming <sofamingalieva@gmail.com>
2025-03-19 19:44:57 +0300

    Create library.cpp

---
+++ timer_manager/library.cpp
@@ -0,0 +1,52 @@
+#include "library.hpp"
+#include <iostream>
+
+
+    void EventScheduler::add_event(int time, const std::string& event) {
+        events[time] = event;
+        time_queue.push(time);
+    }
+
+    std::string EventScheduler::process_next() {
+        if (time_queue.empty()) {
+            return "";
+        }
+
+        int next_time = time_queue.top();
+        time_queue.pop();
+
+        std::string event = events[next_time];
+        events.erase(next_time);
+
+        return event;
+    }
+    bool EventScheduler::is_empty() const {
+        return time_queue.empty();
+    }
+
+    int main() {
+        //Basic
+        EventScheduler es;
+        es.add_event(5, "Backup");
+        es.add_event(2, "Update");
+        es.add_event(10, "Restart");
+
+        //std::cout << "Processing events:\n";
+        while (!es.is_empty()) {
+            std::string event = es.process_next();
+            std::cout << event << "\n";
+        }
+
+        std::cout << "\n";
+
+        //DublicateTimes 
+        EventScheduler es2;
+        es2.add_event(5, "Backup");
+        es2.add_event(5, "Update");
+
+        //std::cout << "Processing events:\n";
+        while (!es2.is_empty()) {
+            std::string event = es2.process_next();
+            std::cout << event << "\n";
+        }
+    }
